d3f45b5bb49e566dc317ae87a2bc63970f9f8b23
Victor Quirino <victor@movenext.com.br>
2016-10-02 15:08:47 -0300

    Finished task 1

--- js/weather.controller.js
+++ js/weather.controller.js
@@ -2,23 +2,58 @@ var task1 = angular.module('task1', ['task1_service']);
 
 task1.controller('GeoCtrl', function($scope, $window, WeatherService){
   var response = $window.confirm('This page wants to get you location. Do you alow?');
-  $scope.geoLocationCoords = false;
+  $scope.loadingView = false;
+
+  $scope.$on('getWeatherInit', function(){
+    $scope.loadingView = true;
+  });
+
+  $scope.$on('getWeatherFinished', function(){
+    $scope.loadingView = false;
+  });
 
   if(response){
+    $scope.$emit('getWeatherInit');
+    $scope.geoLocationCoords = true;
+
     navigator.geolocation.getCurrentPosition(function (position) {
       lat = position.coords.latitude;
       lon = position.coords.longitude;
 
-      WeatherService.getWeatherData(lat, lon)
+      WeatherService.getWeatherDataByCoords(lat, lon)
         .then(function (data){
-          console.log(data);
+          $scope.nameCity = data.name;
+          $scope.weather = data.weather[0].main;
+          $scope.weatherDetails = data.weather[0].description;
+          $scope.country = data.sys.country;
         })
         .catch(function (error){
-
+          $scope.errorMsg = 'We are having some troubles, please try again later.'
+        })
+        .finally(function(){
+          $scope.$emit('getWeatherFinished');
         });
       });
-      $scope.geoLocationCoords = true;
   } else{
       $scope.geoLocationCoords = false;
+  }
+
+  $scope.getWeatherByCountry = function(){
+    $scope.$emit('getWeatherInit');
+    $scope.PostalCodeCountry;
+
+    WeatherService.getWeatherDataByCountry($scope.PostalCodeCountry)
+      .then(function (data){
+        $scope.nameCity = data.name;
+        $scope.weather = data.weather[0].main;
+        $scope.weatherDetails = data.weather[0].description;
+        $scope.country = data.sys.country;
+      })
+      .catch(function (error){
+        $scope.errorMsg = 'We are having some troubles, please try again later.'
+      })
+      .finally(function(){
+        $scope.$emit('getWeatherFinished');
+      });
   }
 });
--- js/weather.services.js
+++ js/weather.services.js
@@ -3,7 +3,7 @@ var task1_service = angular.module('task1_service', ['task1_constants']);
 task1_service.service('WeatherService', function ($q, $http, APIConstants) {
     var self = this;
 
-    this.getWeatherData = function (lat, lon) {
+    this.getWeatherDataByCoords = function (lat, lon) {
         var dfd = $q.defer();
         var url = APIConstants.API_URL + '?lat=' + lat + '&lon=' + lon + '&APPID=' + APIConstants.API_USER_KEY;
 
@@ -17,4 +17,19 @@ task1_service.service('WeatherService', function ($q, $http, APIConstants) {
 
         return dfd.promise;
     };
+
+    this.getWeatherDataByCountry = function (location) {
+      var dfd = $q.defer();
+      var url = APIConstants.API_URL + '?zip=' + location + '&APPID=' + APIConstants.API_USER_KEY;
+
+      $http.get(url, {headers: {"Content-Type": "application/json"}})
+          .success(function (data) {
+              dfd.resolve(data);
+          })
+          .error(function (data, status, headers) {
+              dfd.reject({"data": data, "status": status, "headers": headers});
+          });
+
+      return dfd.promise;
+    }
 });
